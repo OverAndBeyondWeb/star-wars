d861f0c82c0798f1d7bca3ffceddec730fb0c338
Juan Fox <foxj0875@gmail.com>
2017-04-15 23:41:34 -0400

    api calls working

--- app/app.js
+++ app/app.js
@@ -1,11 +1,47 @@
 var express = require('express');
-
+var request = require('request');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('<h1>How Bout Dem Cars</h1>');
-})
+app.set('port', process.env.PORT || 3000);
+app.set('food', 'pizza');
 
-var server = app.listen(3000, function() {
-    console.log('lisening on port 3000');
+app.get('/', function(req, res) {
+
+    /*/!*SWAPI  data*!/
+    var url = 'http://swapi.co/api/planets';
+
+    request(url, function(err, resp, body) {
+
+        data = JSON.parse(body);
+        console.log(data);
+        var info = '';
+
+        data.results.forEach(function(item) {
+            info += '<li><h2>' + item.name + '</h2><p>' + item.population + '</p></li>';
+        })
+        res.send('<h1>How Bout Dem Cars</h1>' + info);
+
+    });*/
+
+    /*Edmunds data*/
+    var url = 'https://api.edmunds.com/api/vehicle/v2/makes?state=used&year=2014&view=basic&fmt=json&api_key=abeazrq5k5nbpeq68tpu9be9';
+
+    request(url, function(err, resp, body) {
+
+        data = JSON.parse(body);
+        console.log(data);
+        var info = '';
+
+        data.makes.forEach(function(item) {
+            info += '<li><h2>' + item.name + '</h2><p>' + item.models[0].name + '</p></li>';
+        })
+        res.send('<h1>How Bout Dem Cars</h1>' + info);
+
+    });
+
+});
+
+
+var server = app.listen(app.get('port'), function() {
+    console.log('listening on port ' + app.get('port'));
 });
